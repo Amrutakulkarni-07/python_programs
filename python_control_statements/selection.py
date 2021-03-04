77984ac8418ae87d337412c8c235535698cb007e
Amruta Kulkarni <74787831+Amrutakulkarni-07@users.noreply.github.com>
2021-03-04 17:39:14 +0530

    Delete python_control_statements directory

--- python_control_statements/selection.py
+++
@@ -1,7 +0,0 @@
-# demonstration of selection
-no=11
-
-if no % 2==0:
-    print("no is even")
-else:
-    print("no is odd")
--- python_control_statements/sequence.py
+++
@@ -1,9 +0,0 @@
-#demonstration of sequence
-
-def fun():
-    print("first line of funtion ")
-    print("second line of function")
-
-print("first line of application")
-fun()
-print("last line of application")
